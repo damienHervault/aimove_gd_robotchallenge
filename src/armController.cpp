fb304af4fb396e040dd84d881aed65473a2f3283
damienHervault <damien.hervault@gmail.com>
2021-01-05 11:55:25 +0100

    Todo list

--- src/armController.cpp
+++ src/armController.cpp
@@ -3,4 +3,14 @@
 int main(int argc, char* argv[])
 {
   ros::init(argc, argv, "armController : au boulot les glandeurs !!");
+
+  /*
+    TODO list
+    - Basics :
+    -- trouver comment dupper les logs dans la console
+    -- trouver comment écouter le node ros_openpose
+    -- trouver comment écrire dans moveit
+    - Sujet :
+    -- trouver comment faire la transformation repère camera > repère table
+  */
 }
