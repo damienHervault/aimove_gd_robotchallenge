0fa12dceff43df4a2b5c155ded102b2ad31e70d2
damienHervault <damien.hervault@gmail.com>
2021-01-05 19:32:41 +0100

    Preparation pour calibration

--- src/armController.cpp
+++ src/armController.cpp
@@ -1,5 +1,6 @@
 #include <armController.hpp>
 #include <cameraReader.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 void callbackRosOpenpose(const ros_openpose::Frame msg){
   //ROS_INFO pour communiquer avec classe dans le cmd
@@ -8,39 +9,57 @@ void callbackRosOpenpose(const ros_openpose::Frame msg){
   //point correspond au coordonnées 3D du bodyPart
 }
 
-int main(int argc, char* argv[])
-{
+void calibrate(std::shared_ptr<CameraReader> readers) {
+  ros::Rate loopRate(10);
+  bool calibrated = false;
+  while (!calibrated)
+  {
+    auto depthImage = readers->getDepthFrame();
+    if (!depthImage.empty()) {
+      calibrated = true;
+      cv::imshow("depth image", depthImage);
+
+      // TODO call D-KHT
+    }
+    else
+      ROS_WARN_THROTTLE(10, "Empty depth image frame detected. Waiting...");
+    ros::spinOnce();
+    loopRate.sleep();
+  }
+}
+
+/*
+  TODO list
+  - Basics :
+  -- trouver comment dupper les logs dans la console: done
+  -- trouver comment écouter le node ros_openpose: done
+  -- trouver comment écrire dans moveit
+  - Sujet :
+  -- trouver comment faire la transformation repère camera > repère table
+*/
+
+int main(int argc, char* argv[]) {
   ros::init(argc, argv, "armController : au boulot les glandeurs !!");
   ros::NodeHandle nh;
 
-  //frame est le nom du rostopic dans lequelle rosOpenpose publie son msg
-  ros::Subscriber counter1_sub = nh.subscribe("frame", 10, callbackRosOpenpose);
-  //std::cout<<"hello"<<std::endl;
-
-  //lets all the callbacks get called for your subscribers
-  ros::spin();
-
-  /*
-    TODO list
-    - Basics :
-    -- trouver comment dupper les logs dans la console: done
-    -- trouver comment écouter le node ros_openpose: done
-    -- trouver comment écrire dans moveit
-    - Sujet :
-    -- trouver comment faire la transformation repère camera > repère table
-  */
-
-  const std::string colorTopic = "/camera/color/image_raw";
-  const std::string camInfoTopic = "/camera/color/camera_info";
-  const std::string depthTopic = "/camera/aligned_depth_to_color/image_raw";
-  const auto cameraReader = std::make_shared<CameraReader>(nh, colorTopic, depthTopic, camInfoTopic);
-/*#if TRUE
+  std::cout<<"Calibration"<<std::endl;
+#if TRUE
+  // realsense
   std::string colorTopic   = "/camera/color/image_raw";
   std::string depthTopic   = "/camera/aligned_depth_to_color/image_raw";
   std::string camInfoTopic = "/camera/color/camera_info";
 #else
+  // kinect
   std::string colorTopic   = "/kinect2/sd/image_color_rect";
   std::string depthTopic   = "/kinect2/sd/image_depth";
   std::string camInfoTopic = "/kinect2/sd/camera_info";
-#endif*/
+#endif
+  const auto cameraReader = std::make_shared<CameraReader>(nh, colorTopic, depthTopic, camInfoTopic);
+  calibrate(cameraReader);
+
+  //frame est le nom du rostopic dans lequelle rosOpenpose publie son msg
+  ros::Subscriber counter1_sub = nh.subscribe("frame", 10, callbackRosOpenpose);
+
+  //lets all the callbacks get called for your subscribers
+  ros::spin();
 }
